fb75371f73e1f55636a47dbdaaf09bc2f2fdd899
Nigel Watson <nigel.watson@vaderfire.com>
2020-05-20 14:06:44 +0100

    gitignore

--- long.js
+++ long.js
@@ -1,17 +1,17 @@
-const Request = require('axios-request-handler').default;
+// const Request = require('axios-request-handler').default;
 
-// console.log(Request);
+// // console.log(Request);
 
-const requestInstance = new Request('http://localhost:3000/pods', {
-	lockable: true,
-	cancelable: true,
-	errorHandler: (error, method) => {
-		console.error(error);
-	}
-});
+// const requestInstance = new Request('http://localhost:3000/pods', {
+// 	lockable: true,
+// 	cancelable: true,
+// 	errorHandler: (error, method) => {
+// 		console.error(error);
+// 	}
+// });
 
-requestInstance
-	.poll(2000)
-	.get(response => {
-		console.log(response);
-	});
+// requestInstance
+// 	.poll(2000)
+// 	.get(response => {
+// 		console.log(response);
+// 	});
